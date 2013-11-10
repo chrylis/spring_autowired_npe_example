80cfa37876d9e60329927772bbdb858447f67f88
Christopher Smith <chrylis@gmail.com>
2013-11-10 17:43:47 -0600

    added @EnableSpringConfigured

--- spring-autowired-npe/src/main/java/com/chrylis/example/spring_autowired_npe/Main.java
+++ spring-autowired-npe/src/main/java/com/chrylis/example/spring_autowired_npe/Main.java
@@ -3,8 +3,10 @@ package com.chrylis.example.spring_autowired_npe;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.aspectj.EnableSpringConfigured;
 
 @EnableAutoConfiguration
+@EnableSpringConfigured
 @ComponentScan
 public class Main {
 	public static void main(String[] args) {
