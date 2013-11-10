59c730991582f615858f4b701cc18f86d2355001
Christopher Smith <chrylis@gmail.com>
2013-11-10 17:18:32 -0600

    compiles but won't run properly, probably because of SPR-5156

--- spring-autowired-npe/src/main/java/com/chrylis/example/spring_autowired_npe/MileageFeeCalculator.java
+++ spring-autowired-npe/src/main/java/com/chrylis/example/spring_autowired_npe/MileageFeeCalculator.java
@@ -1,9 +1,11 @@
 package com.chrylis.example.spring_autowired_npe;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.stereotype.Service;
 
 @Service
+@Configurable
 public class MileageFeeCalculator {
 	
 	@Autowired
