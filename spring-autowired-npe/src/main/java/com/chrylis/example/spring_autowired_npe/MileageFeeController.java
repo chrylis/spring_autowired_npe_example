e27dd94695b14244e3e14ce8be1057224e98eb76
Christopher Smith <chrylis@gmail.com>
2013-11-10 16:49:43 -0600

    This version works because Spring is autowiring the MileageFeeCalculator into the controller and knows to configure it first.

--- spring-autowired-npe/src/main/java/com/chrylis/example/spring_autowired_npe/MileageFeeController.java
+++ spring-autowired-npe/src/main/java/com/chrylis/example/spring_autowired_npe/MileageFeeController.java
@@ -1,16 +1,20 @@
 package com.chrylis.example.spring_autowired_npe;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class MileageFeeController {	
+public class MileageFeeController {
+	
+	@Autowired
+	private MileageFeeCalculator calc;
+	
 	@RequestMapping("/mileage/{miles}")
 	@ResponseBody
 	public float mileageFee(@PathVariable int miles) {
-		MileageFeeCalculator calc = new MileageFeeCalculator();
 		return calc.mileageCharge(miles);
 	}
 }
